690af7f74bdbe4a1bc067c98bcf794c4d64b1847
Prince Anuragi <princanurag07@gmail.com>
2021-06-22 08:02:35 +0530

    Use production config from Heroku ENV variables and decrease socket timeout

--- lib/chat_web/endpoint.ex
+++ lib/chat_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule ChatWeb.Endpoint do
   ]
 
   socket "/socket", ChatWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
